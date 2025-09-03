d26f5ab39d0fd6cd2a52cc590ce26c84035ad166
Mohan Raj <mohanraj123.mrl@gmail.com>
2025-09-03 18:40:41 +0100

    imported icons in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <main>
         <div>
-            <h1>Welcome to my 3D portfolio!</h1>
+            <h1 className='text-3xl underline'>Welcome to my 3D portfolio!
+
+            </h1>
         </div>
     </main>
   )
